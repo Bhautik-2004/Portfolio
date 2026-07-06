2041f3b39c989356b42e48fbc8f941a307ee5113
Bhautik-2004 <bhautikvaghamshi81@gmail.com>
2026-07-06 23:27:00 +0530

    refactor: simplify TweetComponent by removing unnecessary async logic and imports

--- app/components/tweet.tsx
+++ app/components/tweet.tsx
@@ -1,37 +1,16 @@
-import { getTweet } from "react-tweet/api";
-import { EmbeddedTweet, TweetNotFound, type TweetProps } from "react-tweet";
-import "./tweet.css";
+"use client";
 
-const TweetContent = async ({ id, components, onError }: TweetProps) => {
-  let error;
-  const tweet = id
-    ? await getTweet(id).catch((err) => {
-        if (onError) {
-          error = onError(err);
-        } else {
-          console.error(err);
-          error = err;
-        }
-      })
-    : undefined;
+import { Tweet } from "react-tweet";
 
-  if (!tweet) {
-    const NotFound = components?.TweetNotFound || TweetNotFound;
-    return <NotFound error={error} />;
+export function TweetComponent({ id }: { id: string }) {
+  if (!id) {
+    return null;
   }
 
-  return <EmbeddedTweet tweet={tweet} components={components} />;
-};
-
-export const ReactTweet = (props: TweetProps) => <TweetContent {...props} />;
-
-export async function TweetComponent({ id }: { id: string }) {
   return (
     <div className="tweet my-6">
       <div className={`flex justify-center`}>
-        {/* <Suspense fallback={<TweetSkeleton />}> */}
-        <ReactTweet id={id} />
-        {/* </Suspense> */}
+        <Tweet id={id} />
       </div>
     </div>
   );
